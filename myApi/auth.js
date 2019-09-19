88febf1ed6152080d5de2d5a3b0817bdf613098e
jvan1997 <jonathanvan.1997@gmail.com>
2019-09-18 22:13:32 -0700

    relocated files

--- myApi/auth.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.send('API is working properly');
-});
-
-module.export = router;
