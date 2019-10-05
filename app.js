0aab9ed3cc26512e653742793e260ccc88c9c227
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-05 02:13:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ var testAPI = require('./routes/testAPI');
 var posts = require('./routes/postrelated/posts');
 var app = express();
 app.use(cors())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
