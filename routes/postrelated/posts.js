9987a4cd4de8b3431b5a99da28ae22b0fe484ec4
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-05 02:27:18 -0700

    Revert "Added create end point"
    This reverts commit e15889786198c40b4ee8389e3409ab8012e39c68.

--- routes/postrelated/posts.js
+++ routes/postrelated/posts.js
@@ -5,23 +5,4 @@ router.get('/', function(req, res, next) {
     res.send('Post api is working properly');
 });
 
-router.post('/create', async function(req, res) {
-    author = req.body.author
-    bookname = req.body.bookname
-    condition = req.body.condition
-    course = req.body.course
-    description = req.body.description
-    image = req.body.image
-    price = req.body.price
-    poster = req.body.poster;
-    queryString = `INSERT INTO posts (\`book\`,\`author\`,\`course\`,\`condition\`,\`body\`,\`image\`,\`price\`, \`seller\`,\`date\`) \
-    values(${bookname}, ${author}, ${condition},${course},${description},${image},${price},${poster},CONVERT(DATE, GETDATE()))`;
-    connection.query(
-        queryString,
-        function(error, results, fields) {
-          if (error) throw error;
-          res.json(results);
-        }
-      );
-    });
 module.exports = router;
