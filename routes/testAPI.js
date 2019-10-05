f958dbc55e2e8c5ec6de53d5af383f3b2ac9c3e8
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-05 06:50:08 -0700

    refined search

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -15,10 +15,22 @@ router.get('/tables',function(req, res, next) {
       }
     );
   });
-  router.get('/search',function(req, res, next) {
+  router.get('/search', async (req, res)=>{
     console.log("hi");
+    let course =  req.query.course;
+    let name = req.query.name;
+    var conditions ='';
+    if(course !== '' && name !== ''){
+        conditions = 'WHERE tb1.course = ' + course +' AND tb1.book =' + name;
+    }
+    else if(course !== '' && name === ''){
+      conditions = 'WHERE tb1.course = ' + course;
+    }
+    else if(course === '' && name !== ''){
+      conditions = 'WHERE tb1.book = ' + name;
+    }
   connection.query(
-    "SELECT tb1.*, tb2.firstname, tb2.surname FROM posts tb1 JOIN user tb2 on tb1.seller = tb2.iduser;",
+    `SELECT tb1.*, tb2.firstname, tb2.surname FROM posts tb1 JOIN user tb2 on tb1.seller = tb2.iduser${conditions};`,
     function(error, results, fields) {
       if (error) throw error;
       res.json(results);
