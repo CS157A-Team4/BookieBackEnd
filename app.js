342a7322e8268b724e8fb9a55050288255e69dd1
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-05 01:45:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var testAPI = require('./routes/testAPI');
 var posts = require('./routes/postrelated/posts');
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPI);
