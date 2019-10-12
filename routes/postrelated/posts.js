344630c4b1efb3687af576f90823c840f14a37e8
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-12 01:33:05 -0700

    added temporary post

--- routes/postrelated/posts.js
+++ routes/postrelated/posts.js
@@ -4,7 +4,19 @@ var connection = require('../database');
 router.get('/', function(req, res, next) {
     res.send('Post api is working properly');
 });
-
+router.get('/:id', async (req, res) =>{
+  let id =  req.params.id;
+  let queryString = `SELECT * FROM posts WHERE idposts=${id};`;
+  connection.query(queryString,
+    function(error, results, fields) {
+      if (error){
+          console.log(error);
+      }
+      else{
+      res.json(results)};
+    }
+  );
+});
 router.post('/create', async function(req, res) {
     console.log("okay im here");
     author = req.body.author
