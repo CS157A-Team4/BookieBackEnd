25ba1d9ed176f68ddc6455bf20e59b7e21963b5e
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-14 20:36:44 -0700

    syntax for multiple

--- routes/postrelated/posts.js
+++ routes/postrelated/posts.js
@@ -8,7 +8,7 @@ router.get('/:id', async (req, res) =>{
   let id =  req.params.id;
   let queryString = `SELECT tb1.*, tb2.firstname, tb2.surname FROM posts tb1 JOIN user tb2 on tb1.seller = tb2.iduser WHERE idposts=${id};`;
   let commentString= `SELECT * FROM comments WHERE postid=${id};`;
-  connection.query(queryString,commentString,
+  connection.query(queryString+commentString,
     function(error, results, fields) {
       if (error){
           console.log(error);
