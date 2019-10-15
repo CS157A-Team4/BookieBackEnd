5c0f53b5c4245f7faa563d7933fe727133f09d16
jvan1997 <jonathanvan.1997@gmail.com>
2019-10-15 03:01:26 -0700

    bad parenthesis

--- routes/profile/posts.js
+++ routes/profile/posts.js
@@ -36,12 +36,13 @@ router.post('/save', async function(req, res) {
         userId = req.body.userId
         postId = req.body.postId;
         //checkString = `SELECT * FROM SavedPost WHERE \`userId\`="${userId}" AND \`ID_POST\="${postId};`
-        queryString = `DELETE FROM SavedPost WHERE \`userID\` ="${userId}" AND  \`postType\` ="favorite" AND \`ID_Post\`="${postId}");`;
+        queryString = `DELETE FROM SavedPost WHERE \`userID\`="${userId}" AND \`postType\` ="favorite" AND \`ID_Post\`="${postId}";`;
         console.log(queryString);
         connection.query(
             queryString,
             function(error, results, fields) {
               if (error){
+                  console.log(error);
                 return res.status(400).json({
                     error: true,
                     message: "Error Deleting the Saved Posts"
